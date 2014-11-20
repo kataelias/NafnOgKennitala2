0b6e3a80c3e47ba66742708c63e96f595d717156
kataelias <kataelias@hotmail.com>
2014-11-20 17:51:04 +0000

    Homescreen takki kóði(virkar) + Lockscreen takki kóði

--- app/src/main/java/EmergencyButtonAppWidgetProvider.java
+++ app/src/main/java/EmergencyButtonAppWidgetProvider.java
@@ -1,5 +1,5 @@
 
-import myfirstapp.example.com.nafnogkennitala.ExampleActivity;
+import myfirstapp.example.com.nafnogkennitala.MyActivity;
 import myfirstapp.example.com.nafnogkennitala.R;
 
 import android.appwidget.AppWidgetProviderInfo;
@@ -10,42 +10,30 @@ import android.app.PendingIntent;
 import android.widget.RemoteViews;
 import android.appwidget.AppWidgetProvider;
 import android.os.Bundle;
+import android.content.ComponentName;
+import android.content.IntentFilter;
 
 
 /**
  * Created by katrineliasdottir on 09/11/14.
  *
- * Klasi sem sækir stillingar fyrir hnappinn á Home Screen og á Lock Screen.
+ * Klasi sem býr til widget hnapp á Home Screen og á Lock Screen,
+ * sem virkjar MyActivity klasann þegar smellt er á hann.
  */
 
 
 public class EmergencyButtonAppWidgetProvider extends AppWidgetProvider {
 
-    /* private static final String ACTION_CLICK = "ACTION_CLICK";
-    */
-    AppWidgetManager appWidgetManager;
-    int widgetId;
-    Bundle myOptions = appWidgetManager.getAppWidgetOptions (widgetId);
-
-    // Get the value of OPTION_APPWIDGET_HOST_CATEGORY
-    int category = myOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
-
-    // If the value is WIDGET_CATEGORY_KEYGUARD, it's a lockscreen widget
-    boolean isKeyguard = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;
-
-    int baseLayout = isKeyguard ? R.layout.button_keyguard : R.layout.button_appwidget;
-
-
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int N = appWidgetIds.length;
 
         // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < appWidgetIds.length; i++) {
             int appWidgetId = appWidgetIds[i];
 
             // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, ExampleActivity.class);
+            Intent intent = new Intent(context, MyActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
@@ -54,8 +42,10 @@ public class EmergencyButtonAppWidgetProvider extends AppWidgetProvider {
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
-    }
 
+
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
 
 }
--- app/src/main/java/myfirstapp/example/com/nafnogkennitala/ExampleActivity.java
+++ app/src/main/java/myfirstapp/example/com/nafnogkennitala/ExampleActivity.java
@@ -9,5 +9,4 @@ package myfirstapp.example.com.nafnogkennitala;
 public class ExampleActivity {
 
 
-
 }
